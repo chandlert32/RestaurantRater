e7db298e074b1989d441bf9907242dd1d5ec9862
Chandler Tan <tanchandler4@gmail.com>
2020-03-23 17:32:36 -0400

    Had to go back and redo

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -9,11 +9,11 @@ namespace RestaurantRater.Controllers
 {
     public class RestaurantController : Controller
     {
-        private RestaurantDbContext _db = new RestaurantDbContext();
         // GET: Restaurant
         public ActionResult Index()
         {
-            return View(_db.Restaurants.ToList());
+            return View();
         }
+
     }
 }
